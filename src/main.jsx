c29674ffacc7b8508af180f9c25ac29fe4e8678a
emon1995 <emonhossain995@gmail.com>
2023-05-28 13:15:48 +0600

    custom hook useCart added with tanstack

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,17 @@ import { RouterProvider } from "react-router-dom";
 import router from "./Routes/router.jsx";
 import { HelmetProvider } from "react-helmet-async";
 import AuthProviders from "./providers/AuthProviders";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
       <AuthProviders>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </AuthProviders>
     </HelmetProvider>
   </React.StrictMode>
